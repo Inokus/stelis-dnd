14803bb4ccfe173fcabe6a0bba7031d05e2fe61c
Inokus <inokus@outlook.com>
2024-07-25 19:24:32 +0300

    feat: add test database

--- server/src/tests/utils/database.ts
+++ server/src/tests/utils/database.ts
@@ -1,4 +1,4 @@
 import config from '@server/config';
 import { createDatabase } from '@server/database';
 
-export const createTestDatabase = () => createDatabase(config.database);
+export const createTestDatabase = () => createDatabase(config.testDatabase);
